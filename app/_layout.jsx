cd61f4291b93e0439d5f2bb998144e56f4e107a6
LestDomz <lestermartinez108@gmail.com>
2025-09-04 13:29:53 -0700

    Sign In Page Completed
    Multiple new changes!
    I created a context folder that will check if the user is signed in or not.
    On start up, user will not be signed-in and will need to sign in with the button on the top-right.
    We currently have a hard-coded username and password, it will be "Admin" and "passwd", they are case sensitive.
    After user is signed-in, their username will follow them to the home screen.
    Next we will use this name as a button to redirect the user to the user settings page, allowing them to log out.

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,21 +2,46 @@
 import { Stack } from "expo-router";
 import { Pressable, Text } from "react-native";
 import { Link } from "expo-router";
+import { AuthProvider, useAuth } from "../context/AuthContext";
+
+function HeaderRight() {
+  const { user } = useAuth();
+
+  if (user) {
+    return (
+      <Text style={{ marginRight: 10, fontSize: 16, fontWeight: "bold" }}>
+        {user.username}
+      </Text>
+    );
+  }
+
+  //Sign in button set-up
+  return (
+    <Link href="/signIn" asChild>
+      <Pressable>
+        <Text style={{ color: "blue", fontSize: 16, marginRight: 10 }}>
+          Sign In
+        </Text>
+      </Pressable>
+    </Link>
+  );
+}
 
 export default function Layout() {
   return (
-    <Stack
-      screenOptions={{
-        headerRight: () => (
-          <Link href="/signIn" asChild>
-            <Pressable>
-              <Text style={{ color: "blue", fontSize: 16, marginRight: 10 }}>
-                Sign In
-              </Text>
-            </Pressable>
-          </Link>
-        ),
-      }}
-    />
+    <AuthProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ headerRight: () => <HeaderRight /> }}
+        />
+        <Stack.Screen
+          name="empty_page"
+          options={{ headerRight: () => <HeaderRight /> }}
+        />
+        {/*Sign in button at top-right*/}
+        <Stack.Screen name="signIn" options={{ headerRight: () => null }} />
+      </Stack>
+    </AuthProvider>
   );
 }

386043deeff786b6fc218b5fbbc783ad7d1f4063
Tyler Pruitt <tpruitt333@gmail.com>
2025-09-10 19:28:39 -0700

    Managed to pull the number for the most recent comic and use that as an upper bound for pulling a random comic

--- app/Api-pull.jsx
+++ app/Api-pull.jsx
@@ -1,16 +1,35 @@
-import { StyleSheet, ActivityIndicator, FlatList, Text, View, Image } from 'react-native';
+import { StyleSheet, ActivityIndicator, FlatList, View, Image, Text, Button} from 'react-native';
 import { Link } from 'expo-router';
 import { useEffect, useState } from 'react';
+
+var recentComicNum = 0;
 
 const Page = () => {
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
+    
 
     const getTodaysComic = async () => {
         try {
-            const response = await fetch('https://xkcd.com/613/info.0.json');
+            const response = await fetch('https://xkcd.com/info.0.json');
             const json = await response.json();
             setData([json]);
+            recentComicNum = json.num;
+            console.log(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const getRandomComic = async () => {
+        try {
+            var randomNum = Math.floor(Math.random() * recentComicNum) + 1;
+            const response = await fetch(`https://xkcd.com/${randomNum}/info.0.json`);
+            const json = await response.json();
+            setData([json]);
+            console.log(json);
         } catch (error) {
             console.error(error);
         } finally {
@@ -27,9 +46,13 @@ const Page = () => {
             {isLoading ? <ActivityIndicator /> : (
                 <FlatList
                     data={data}
-                    keyExtractor={({ id }, index) => id}
+                    keyExtractor={({ id }) => id}
                     renderItem={({ item }) => (
-                        <Image source={{ uri: item.img }} style={styles.image} />
+                        <View>
+                            <Text>{item.title}</Text>
+                            <Image source={{ uri: item.img }} style={styles.image} />
+                            <Button title="Get Random Comic" onPress={() => getRandomComic()} />
+                        </View>
                     )}
                 />
             )}
@@ -42,7 +65,8 @@ export default Page;
 
 
 const styles = StyleSheet.create({
-    image: { width: 600, height: 300, marginBottom: 20 },
+    FlatList:{ flex: 1, width: '100%' },
+    image: { width: 350, height: 300, marginBottom: 20 ,resizeMode:'contain',},
     container: {
         flex: 1,
         padding: 24,

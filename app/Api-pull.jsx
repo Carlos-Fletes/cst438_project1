5088d0399550a0b454477689316c7c610c74448c
Tyler Pruitt <tpruitt333@gmail.com>
2025-09-10 18:26:53 -0700

    Got an image showing, have yet to get it to update with different links, testing options

--- app/Api-pull.jsx
+++ app/Api-pull.jsx
@@ -2,47 +2,59 @@ import { StyleSheet, ActivityIndicator, FlatList, Text, View, Image } from 'reac
 import { Link } from 'expo-router';
 import { useEffect, useState } from 'react';
 
-const Page = async () => {
+const Page = () => {
     const [isLoading, setLoading] = useState(true);
-    const [todaysComic, comic, setComic] = useState([]);
+    const [data, setData] = useState([]);
 
     const getTodaysComic = async () => {
-        return fetch('https://xkcd.com/info.0.json')
-            .then((response) => response.json())
-            .then((json) => {
-                return json.num;
-            });
-    };
-
-    const getComicData = async (comicNum) => {
-        return fetch(`https://xkcd.com/${comicNum}/info.0.json`)
-            .then((response) => response.json())
-            .then((json) => {
-                return json;
-            });
+        try {
+            const response = await fetch('https://xkcd.com/613/info.0.json');
+            const json = await response.json();
+            setData([json]);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
     };
 
     useEffect(() => {
-        getTodaysComic()
-            .then((num) => getComicData(num))
-            .then((data) => setComic(data))
-            .catch((error) => console.error(error))
-            .finally(() => setLoading(false));
+        getTodaysComic();
     }, []);
-
 
     return (
         <View style={styles.container}>
-
-            <Text style={styles.text}>API Data:</Text>
-            <Image
-                source={{ uri: `https://xkcd.com/${(await getComicData()).num}/info.0.json` }}
-                style={styles.image}
-            />
+            {isLoading ? <ActivityIndicator /> : (
+                <FlatList
+                    data={data}
+                    keyExtractor={({ id }, index) => id}
+                    renderItem={({ item }) => (
+                        <Image source={{ uri: item.img }} style={styles.image} />
+                    )}
+                />
+            )}
             <Link href="/empty_page">Go to Empty Page(This is link)</Link>
         </View>
     );
 }
 
 export default Page;
-const styles = StyleSheet.create({});
+
+
+const styles = StyleSheet.create({
+    image: { width: 600, height: 300, marginBottom: 20 },
+    container: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 20,
+        marginBottom: 20,
+    },
+    link: {
+        marginTop: 20,
+        color: 'blue',
+    },
+});
--- app/index.jsx
+++ app/index.jsx
@@ -37,6 +37,10 @@ export default function IndexPage() {
       <Link href="/empty_page" style={styles.link}>
         Go to Empty Page (This is link)
       </Link>
+
+      <Link href="/Api-pull" style={styles.link}>
+        Go to API Pull Page (This is link)
+      </Link>
     </View>
   );
 }
